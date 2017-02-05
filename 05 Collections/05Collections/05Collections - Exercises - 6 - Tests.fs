33d5d7e642be6d4d51abb2659aee10c450fc8b56
stefanpu <stefanpu13@gmail.com>
2017-02-05 17:45:55 +0200

    Add implementation and tests of "domain" function

--- 05 Collections/05Collections/05Collections - Exercises - 6 - Tests.fs
+++ 05 Collections/05Collections/05Collections - Exercises - 6 - Tests.fs
@@ -3,11 +3,27 @@ open NUnit.Framework
 open FsUnitTyped
 open Exercises6.E
 
-// dom tests
+// domain tests
 [<TestFixture>]
-type ``dom tests``() =
+type ``domain tests``() =
     [<Test>]
-    member t.``colMap tests`` () = 
-        dom  2 |> shouldEqual true
+    member t.``If relation contains person first names "Peter" and Steve", the result should be set ["Peter";"Steve"] `` () = 
+        let firstNames = set ["Steve"; "Peter"; "John"; "Michael";"Gregory"]
+        let lastNames = set ["Wonder"; "Milton"; "Ford"; "Garden"]
+        let personNames = set [("Peter", "Milton"); ("Peter","Garden"); ("Steve","Wonder")]
+        domain  personNames firstNames lastNames |> shouldEqual (set ["Peter";"Steve"])
+    
+    [<Test>]
+    member t.``If relation is empty, the result should be set [] `` () = 
+        let firstNames = set ["Steve"; "Peter"; "John"; "Michael";"Gregory"]
+        let lastNames = set ["Wonder"; "Milton"; "Ford"; "Garden"]
+        let personNames = set []
+        domain  personNames firstNames lastNames |> shouldEqual (set [])
 
+    [<Test>]
+    member t.``If relation contains "Peter" and "John", the result should be set ["Peter"; "John"] `` () = 
+        let names = set ["Steve"; "Peter"; "John"; "Michael";"Gregory"]
+        let olympicGames = set [("Rio", 2016); ("Pekin", 2008); ("London", 2012); ("Athens", 2004)]
+        let participations = set [("Peter", ("Pekin", 2008);); ("Peter", ("London", 2012);); ("John", ("London", 2012);)]
+        domain  participations names olympicGames |> shouldEqual (set ["Peter"; "John"])
     
--- 05 Collections/05Collections/05Collections - Exercises - 6.fs
+++ 05 Collections/05Collections/05Collections - Exercises - 6.fs
@@ -1,3 +1,17 @@
 namespace Exercises6
 module E = 
-    let dom x = true
+    (* 5.6
+        We define a relation from a set A to a set B as a subset of A × B. A relation r
+        is said to be smaller than r, if r is a subset of r, that is, if r ⊆ r. 
+        A relation r is called finite if it is a finite subset of A × B. 
+        Assuming that the sets A and B are represented by F# types ’a and ’b
+        allowing comparison we can represent a finite relation r by a value of type set<’a * ’b>.
+    *)
+
+    (* 5.6.1
+        The domain dom r of a relation r is the set of elements a in A where there exists an element
+        b in B such that (a, b) ∈ r. Write an F# declaration expressing the domain function.
+    *)
+    let domain relation setA setB = 
+        setA |> 
+            Set.filter (fun elA -> setB |> Set.exists (fun elB -> Set.contains (elA, elB) relation))
