f309d684c0fa45925cf6a730170c842f4ca69d20
stefanpu <stefanpu13@gmail.com>
2017-02-16 09:10:10 +0200

    Add tests to exercise 5.7

--- 05 Collections/05Collections/05Collections - Exercises - 6 - Tests.fs
+++ 05 Collections/05Collections/05Collections - Exercises - 6 - Tests.fs
@@ -187,3 +187,28 @@ type ``transitive relation closure tests``() =
         let r = Set [(2,1)]
         
         transitiveClosure r|> shouldEqual (Set [(2,1)])
+
+//all subsets tests
+[<TestFixture>]
+type ``all subsets tests``() =
+    [<Test>]
+    member t.``If n=2 k=1, then result should be [[1];[2]]`` () = 
+        allSubsets 2 1 |> shouldEqual [[1];[2]]
+
+    [<Test>]
+    member t.``If n=3 k=2, then result should be [[1;2];[1;3];[2;3]]`` () = 
+        allSubsets 3 2 |> shouldEqual [[1;2];[1;3];[2;3]]
+
+    [<Test>]
+    member t.``If n=random k=random, then result should be [[1;2];[1;3];[2;3]]`` () = 
+        let rand = System.Random()
+        let x = rand.Next(1, 20)
+        allSubsets x x |> shouldEqual [[1..x]]
+
+    [<Test>]
+    member t.``If n=random k=random+1, then result should be []`` () = 
+        let rand = System.Random()
+        let x = rand.Next(1, 20)
+        allSubsets x (x + 1) |> shouldEqual []
+
+    
