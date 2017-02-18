a54605de29e919252155ef166e0e65027e4288b9
stefanpu <stefanpu13@gmail.com>
2017-02-18 17:15:24 +0200

    Remove unnecessary type declarations

--- 05 Collections/05Collections/05Collections - Exercises - 8.fs
+++ 05 Collections/05Collections/05Collections - Exercises - 8.fs
@@ -3,15 +3,6 @@ module E =
     (* 5.8
         Give declarations for makeBill3 using map.fold rather than map.foldBack.
     *)
-    type ArticleCode =ArticleCode of string
-    type ArticleName = ArticleName of string
-    type NoPieces = NoPieces of int
-    type Price = Price of int
-    type Register = Register of Map<ArticleCode, ArticleName*Price>
-    type Purchase = Purchase of Map<ArticleCode,NoPieces>
-    type Info =  Info of NoPieces * ArticleName * Price
-    type Infoseq = Infoseq of Info list
-    type Bill = Bill of Infoseq * Price
 
     let makeBill3FoldBack  reg pur =
         let f ac np (infos, billprice) =
