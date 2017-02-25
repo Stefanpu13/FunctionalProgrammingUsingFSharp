76c55d345e663102ccf9e91ad5e4c5568c8911b7
stefanpu <stefanpu13@gmail.com>
2017-02-25 19:54:50 +0200

    Solution to exercise 1 and tests

--- 06 Finite Trees/06FiniteTrees/06FiniteTrees - Exercises - 1 - Tests.fs
+++ 06 Finite Trees/06FiniteTrees/06FiniteTrees - Exercises - 1 - Tests.fs
@@ -5,8 +5,53 @@ open Exercises1.E
 
 // proj setup test
 [<TestFixture>]
-type `` proj setup test``() =
+type ``Test reduce function``() =
     [<Test>]
-    member t.`` proj setup test`` () = 
-        add 2 3 |> shouldEqual 5 
+    member t.``If expression is D(x^2 * x), result should be "Add (Mul (Mul (Const 2.0,X),X),Mul (X,X))"`` () = 
+        reduce (D(Mul(Mul(X, X), X))) |> shouldEqual (Add (Mul (Mul (Const 2.0,X),X),Mul (X,X)))
+
+    [<Test>]
+    member t.``If expression is D(3 * x), result should be "Const 3"`` () = 
+        reduce (D(Mul(Const 3.0, X))) |> shouldEqual (Const 3.0)
+
+    [<Test>]
+    member t.``If expression is (X + X) * 0 or 0 * (X+X), result should be "Const 0.0"`` () = 
+        reduce (Mul(Const 0.0, Add(X, X))) |> shouldEqual (Const 0.0)
+        reduce (Mul(Add(X, X), Const 0.0)) |> shouldEqual (Const 0.0)
+
+    [<Test>]
+    member t.``If expression is 0 + X, or X + 0 , result should be "X"`` () = 
+        reduce (Add(Const 0.0,  X)) |> shouldEqual X  
+        reduce (Add(X, Const 0.0)) |> shouldEqual X  
+
+    [<Test>]
+    member t.``If expression is X * 1 or 1 * X, result should be "X"`` () = 
+        reduce (Mul(Const 1.0,  X)) |> shouldEqual (X)
+        reduce (Mul(X, Const 1.0)) |> shouldEqual (X)
+    
+    [<Test>]
+    member t.``If expression is 2 + 4 , result should be "Const 6.0"`` () = 
+        reduce (Add(Const 2.0, Const 4.0)) |> shouldEqual (Const 6.0)  
+
+    [<Test>]
+    member t.``If expression is 5 * 2, result should be "Const 10.0"`` () = 
+        reduce (Mul(Const 5.0,  Const 2.0)) |> shouldEqual (Const 10.0)    
+    
+    [<Test>]
+    member t.``If expression is X + X , result should be "Mul(Const 2.0, X)"`` () = 
+        reduce (Add(X,  X)) |> shouldEqual (Mul(Const 2.0, X))   
+
+    [<Test>]
+    member t.``If expression is X^2 + X^2 , result should be "Mul(Const 2.0, Mul(X, X))"`` () = 
+        reduce (Add(Mul(X, X),  Mul(X, X))) |> shouldEqual (Mul(Const 2.0, Mul(X, X))) 
+
+    [<Test>]
+    member t.``If expression is X + 2 , result should be "Add(X, Const 2.0)"`` () = 
+        reduce (Add(X,  Const 2.0)) |> shouldEqual (Add(X, Const 2.0))   
+
+    [<Test>]
+    member t.``If expression is D(X/X) , result should be "Const 0.0"`` () = 
+        reduce (D(Div(X, X))) |> shouldEqual (Const 0.0)
+    
+        
 
--- 06 Finite Trees/06FiniteTrees/06FiniteTrees - Exercises - 1.fs
+++ 06 Finite Trees/06FiniteTrees/06FiniteTrees - Exercises - 1.fs
@@ -1,3 +1,66 @@
 namespace Exercises1
 module E = 
-    let add x y = x+y 
+    (* 6.1
+        Declare a function red of type Fexpr -> Fexpr to reduce expressions generated from the
+        differentiation program in Section 6.2. For example, sub-expressions of form Const 1.0 * e
+        can be reduced to e. (A solution is satisfactory if the expression becomes “nicer”. It is difficult
+        to design a reduce function so that all trivial sub-expressions are eliminated.)
+    *)
+
+    type Fexpr = 
+        | Const of float
+        | X
+        | Add of Fexpr * Fexpr
+        | Sub of Fexpr * Fexpr
+        | Mul of Fexpr * Fexpr
+        | Div of Fexpr * Fexpr
+        | Sin of Fexpr
+        | Cos of Fexpr
+        | Log of Fexpr
+        | Exp of Fexpr
+
+    let rec D = function
+        | Const _ -> Const 0.0
+        | X -> Const 1.0
+        | Add(fe,ge) -> Add(D fe, D ge)
+        | Sub(fe,ge) -> Sub(D fe, D ge)
+        | Mul(fe,ge) -> Add(Mul(D fe, ge), Mul(fe, D ge))
+        | Div(fe,ge) -> Div(Sub(Mul(D fe,ge), Mul(fe,D ge)),Mul(ge,ge))
+        | Sin fe -> Mul(Cos fe, D fe)
+        | Cos fe -> Mul(Const -1.0, Mul(Sin fe, D fe))
+        | Log fe -> Div(D fe, fe)
+        | Exp fe -> Mul(Exp fe, D fe)
+
+
+    // very naive solution
+    let rec reduce = function     
+    | Add(exp1, exp2) ->
+        match (exp1, exp2) with
+        | (Const 0.0, exp)| (exp, Const 0.0) -> reduce exp
+        | (Const x, Const y) -> Const (x + y)
+        | (e1, e2) when e1 = e2 -> Mul (Const 2.0, reduce e1)
+        | (e1, e2) when e1 <> reduce e1 || e2 <> reduce e2 -> reduce (Add (reduce exp1, reduce exp2))        
+        | (e1, e2) -> (Add (reduce exp1, reduce exp2))
+    | Mul (exp1, exp2) ->
+        match (exp1, exp2) with
+        | (Const 0.0, exp)| (exp, Const 0.0) -> Const (0.0)
+        | (Const 1.0, exp)| (exp, Const 1.0) -> reduce exp
+        | (Const x, Const y) -> Const (x * y)
+        | (e1, e2) when e1 <> reduce e1 || e2 <> reduce e2 -> reduce (Mul (reduce exp1, reduce exp2))
+        | (e1, e2) -> (Mul (reduce exp1, reduce exp2)) 
+    | Const f -> Const f
+    | X -> X
+    | Sub(exp1, exp2) -> 
+        match exp1, exp2 with 
+        | e1, e2 when e1 = e2 -> Const 0.0
+        | (e1, e2) when e1 <> reduce e1 || e2 <> reduce e2 -> reduce (Sub (reduce exp1, reduce exp2))
+        | exp1, exp2 -> Sub (reduce exp1, reduce exp2)    
+    | Div (exp1, exp2) -> 
+        match (exp1, exp2) with 
+        | Const 0.0, e1 -> Const 0.0 
+        | (e1, e2) when e1 <> reduce e1 || e2 <> reduce e2 -> reduce (Div (reduce exp1, reduce exp2))
+        | e1, e2 -> Div (reduce e1, reduce e2) 
+    | Sin fe -> Sin (reduce fe)
+    | Cos fe -> Cos (reduce fe) 
+    | Log fe -> Log (reduce fe) 
+    | Exp fe -> Exp (reduce fe)
