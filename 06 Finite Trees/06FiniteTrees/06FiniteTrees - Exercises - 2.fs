14d47c327881c573522ea3588cdcd70250da3475
stefanpu <stefanpu13@gmail.com>
2017-03-12 14:12:00 +0200

    Update postfix function from Exercise 2 to use optimised tail recursive version

--- 06 Finite Trees/06FiniteTrees/06FiniteTrees - Exercises - 2.fs
+++ 06 Finite Trees/06FiniteTrees/06FiniteTrees - Exercises - 2.fs
@@ -2,7 +2,7 @@ namespace Exercises2
 module E = 
     open Exercises1Common.Types.Expression
     open Exercises1Common.Utils
-    (* 6.1
+    (* 6.2
         Postfix form is a particular representation of arithmetic expressions where each operator is
         preceded by its operand(s), for example:
         (x + 7.0) has postfix form x 7.0 +
@@ -11,17 +11,34 @@ module E =
         expression trees from Section 6.2.
     *)
 
+    // naive implementation 
+    // let postFix expr = 
+    //     let rec postFix l = function
+    //         | Const x -> x.ToString():: l
+    //         | X -> "x" :: l
+    //         | Add (x, y) -> (postFix l x) @ (postFix l y) @ ["+"]            
+    //         | Sub (x, y) -> (postFix l x) @ (postFix l y) @ ["-"]            
+    //         | Mul (x, y) -> (postFix l x) @ (postFix l y) @ ["*"]            
+    //         | Div (x, y) -> (postFix l x) @ (postFix l y) @ ["/"]            
+    //         | _ -> l
+
+    //     let result = (postFix [] expr) |> List.fold(fun str term -> str + " " + term) ""
+        
+    //     result.Substring(1)       
+
+
     let postFix expr = 
-        let rec postFix l = function
+        let rec postFix el l = 
+            match el with
             | Const x -> x.ToString():: l
             | X -> "x" :: l
-            | Add (x, y) -> (postFix l x) @ (postFix l y) @ ["+"]            
-            | Sub (x, y) -> (postFix l x) @ (postFix l y) @ ["-"]            
-            | Mul (x, y) -> (postFix l x) @ (postFix l y) @ ["*"]            
-            | Div (x, y) -> (postFix l x) @ (postFix l y) @ ["/"]            
+            | Add (x, y) -> postFix x (postFix y ("+"::l))         
+            | Sub (x, y) -> postFix x (postFix y ("-"::l))             
+            | Mul (x, y) -> postFix x (postFix y ("*"::l))             
+            | Div (x, y) -> postFix x (postFix y ("/"::l))              
             | _ -> l
 
-        let result = (postFix [] expr) |> List.fold(fun str term -> str + " " + term) ""
+        let result = (postFix expr []) |> List.fold(fun str term -> str + " " + term) ""
         
-        result.Substring(1)             
+        result.Substring(1)            
     
