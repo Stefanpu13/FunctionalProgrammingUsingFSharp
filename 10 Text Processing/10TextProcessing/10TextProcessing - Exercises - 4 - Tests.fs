6eca47ef91c7c40d55b7fa7bb96ca591e75b98dd
stefanpu <stefanpu13@gmail.com>
2017-05-28 20:49:02 +0300

    Validate degrees and minutes type and range; Add tests to exercise 4;

---
+++ 10 Text Processing/10TextProcessing/10TextProcessing - Exercises - 4 - Tests.fs
@@ -0,0 +1,124 @@
+module Exercises4Tests
+#if INTERACTIVE
+#r @"C:/Programming/Tutorials/FSharp/CodeFromBooks/FPUsingFSharp/10 Text Processing/10TextProcessing/bin/Debug/nunit.framework.dll"
+#r @"C:/Programming/Tutorials/FSharp/CodeFromBooks/FPUsingFSharp/10 Text Processing/10TextProcessing/bin/Debug/FsUnit.NUnit.dll"
+#endif
+open NUnit.Framework
+open System.IO
+open FsUnitTyped
+open Exercises4.E
+
+// Test getPosition
+[<TestFixture>]
+type ``Test getPosition``() =
+
+    [<Test>]
+    member t.``If valid position input is entered, output should be successfully parsed`` () = 
+        let input =  """14*27'35.03" N 55*1'47" E"""
+        getPosition input |> shouldEqual <| 
+            ValidCoords (14.0*60.0*60.0 + 27.0*60.0 + 35.03, 55.0*3600.0 + 1.0*60.0+47.0)
+
+    [<Test>]
+    member t.``If valid input is entered and lat is S and lgt is W, output should parse them as negative numbers`` () = 
+        let input =  """14*59'59.03" S 55*1'47" W"""
+        getPosition input |> shouldEqual <| 
+            ValidCoords (-1.0 * (14.0*60.0*60.0 + 59.0*60.0 + 59.03), -1.0 * (55.0*3600.0 + 1.0*60.0+47.0))
+
+    [<Test>]
+    member t.``If lat and lgt are swapped, result should be invalid`` () = 
+        let validInput =  """14*59'59.03" S 55*1'47" W"""
+        getPosition validInput |> shouldEqual <| 
+            ValidCoords (-1.0 * (14.0*60.0*60.0 + 59.0*60.0 + 59.03), -1.0 * (55.0*3600.0 + 1.0*60.0+47.0))
+        let invalidInput = """14*59'59.03" W 55*1'47" S"""
+        getPosition invalidInput |> shouldEqual <| InvalidCoords
+
+    [<Test>]
+    member t.``If degrees are outside range, result should be invalid`` () = 
+        let validInput =  """14*59'59.03" S 55*1'47" W"""
+        getPosition validInput |> shouldEqual <| 
+            ValidCoords (-1.0 * (14.0*60.0*60.0 + 59.0*60.0 + 59.03), -1.0 * (55.0*3600.0 + 1.0*60.0+47.0))
+        let invalidInputLatSmaller = """-14*59'59.03" S 55*1'47" W"""
+        getPosition invalidInputLatSmaller |> shouldEqual <| InvalidCoords
+
+        let invalidInputLatBigger = """184*59'59.03" N 55*1'47" W"""
+        getPosition invalidInputLatBigger |> shouldEqual <| InvalidCoords
+
+        let invalidInputLgtSmaller = """14*59'59.03" N -55*1'47" E"""
+        getPosition invalidInputLgtSmaller |> shouldEqual <| InvalidCoords
+
+        let invalidInputLgtBigger = """14*59'59.03" S 192*1'47" W"""
+        getPosition invalidInputLgtBigger |> shouldEqual <| InvalidCoords
+
+    [<Test>]
+    member t.``If degrees are not integers, result should be invalid`` () = 
+        let validInput =  """14*59'59.03" S 55*1'47" W"""
+        getPosition validInput |> shouldEqual <| 
+            ValidCoords (-1.0 * (14.0*60.0*60.0 + 59.0*60.0 + 59.03), -1.0 * (55.0*3600.0 + 1.0*60.0+47.0))
+        let invalidInputLatDouble = """1.04*59'59.03" S 55*1'47" W"""
+        getPosition invalidInputLatDouble |> shouldEqual <| InvalidCoords
+
+        let invalidInputLgtDouble = """14*59'59.03" S 12.6*1'47" W"""
+        getPosition invalidInputLgtDouble |> shouldEqual <| InvalidCoords
+
+    [<Test>]
+    member t.``If minutes are outside range, result should be invalid`` () = 
+        let validInput =  """14*59'59.03" S 55*1'47" W"""
+        getPosition validInput |> shouldEqual <| 
+            ValidCoords (-1.0 * (14.0*60.0*60.0 + 59.0*60.0 + 59.03), -1.0 * (55.0*3600.0 + 1.0*60.0+47.0))
+        let invalidInputLatSmaller = """14*-59'59.03" S 55*1'47" W"""
+        getPosition invalidInputLatSmaller |> shouldEqual <| InvalidCoords
+
+        let invalidInputLatBigger = """14*69'59.03" N 55*1'47" W"""
+        getPosition invalidInputLatBigger |> shouldEqual <| InvalidCoords
+
+        let invalidInputLgtSmaller = """14*59'59.03" N 55*-1'47" E"""
+        getPosition invalidInputLgtSmaller |> shouldEqual <| InvalidCoords
+
+        let invalidInputLgtBigger = """14*59'59.03" S 55*61'47" W"""
+        getPosition invalidInputLgtBigger |> shouldEqual <| InvalidCoords
+
+    [<Test>]
+    member t.``If minutes are not integers, result should be invalid`` () = 
+        let validInput =  """14*59'59.03" S 55*1'47" W"""
+        getPosition validInput |> shouldEqual <| 
+            ValidCoords (-1.0 * (14.0*60.0*60.0 + 59.0*60.0 + 59.03), -1.0 * (55.0*3600.0 + 1.0*60.0+47.0))
+        let invalidInputLatDouble = """1.*59.01'59.03" S 55*1'47" W"""
+        getPosition invalidInputLatDouble |> shouldEqual <| InvalidCoords
+
+        let invalidInputLgtDouble = """14*59'59.03" S 12*1.55'47" W"""
+        getPosition invalidInputLgtDouble |> shouldEqual <| InvalidCoords
+
+
+    [<Test>]
+    member t.``If seconds are outside range, result should be invalid`` () = 
+        let validInput =  """14*59'59.03" S 55*1'47" W"""
+        getPosition validInput |> shouldEqual <| 
+            ValidCoords (-1.0 * (14.0*60.0*60.0 + 59.0*60.0 + 59.03), -1.0 * (55.0*3600.0 + 1.0*60.0+47.0))
+        let invalidInputLatSmaller = """14*59'-59.03" S 55*1'47" W"""
+        getPosition invalidInputLatSmaller |> shouldEqual <| InvalidCoords
+
+        let invalidInputLatBigger = """14*59'69.03" N 55*1'47" W"""
+        getPosition invalidInputLatBigger |> shouldEqual <| InvalidCoords
+
+        let invalidInputLgtSmaller = """14*59'59.03" N 55*1'-47" E"""
+        getPosition invalidInputLgtSmaller |> shouldEqual <| InvalidCoords
+
+        let invalidInputLgtBigger = """14*59'59.03" S 55*1'147" W"""
+        getPosition invalidInputLgtBigger |> shouldEqual <| InvalidCoords
+
+    [<Test>]
+    member t.``If letter other than N|S|W|E is used, result should be invalid`` () = 
+        let validInput =  """14*59'59.03" S 55*1'47" W"""
+        getPosition validInput |> shouldEqual <| 
+            ValidCoords (-1.0 * (14.0*60.0*60.0 + 59.0*60.0 + 59.03), -1.0 * (55.0*3600.0 + 1.0*60.0+47.0))
+        let invalidInputLatSmallLetter = """14*59'59.03" s 55*1'47" W"""
+        getPosition invalidInputLatSmallLetter |> shouldEqual <| InvalidCoords
+
+        let invalidInputLgtSmallLetter = """14*59'69.03" N 55*1'47" w"""
+        getPosition invalidInputLgtSmallLetter |> shouldEqual <| InvalidCoords
+
+        let invalidInputLatLetter = """14*59'59.03" a 55*1'-47" E"""
+        getPosition invalidInputLatLetter |> shouldEqual <| InvalidCoords
+
+        let invalidInputLgtBigger = """14*59'59.03" S 55*1'147" x"""
+        getPosition invalidInputLgtBigger |> shouldEqual <| InvalidCoords
--- 10 Text Processing/10TextProcessing/10TextProcessing - Exercises - 4.fs
+++ 10 Text Processing/10TextProcessing/10TextProcessing - Exercises - 4.fs
@@ -1,42 +1,60 @@
-namespace Exercies4
+namespace Exercises4
 open System
 open System.Text.RegularExpressions
 #if INTERACTIVE
 #load "TextProcessing.fs"
 #endif
 open TextProcessing.TextProcessing
-module E =         
-    type ValidPosition = {degrees: float; minutes: float; seconds: float; sign: float}
+module E =    
+    type Direction = E|W|S|N
+    type PositionPart = Degree of string*Direction  | Minute of string | Second of string | Sign of string
+    type ValidPosition = {degrees: int; minutes: int; seconds: float; sign: float}
     type Position =  Lat of ValidPosition  | Lgt of ValidPosition  | InvalidPosition
     type Coord = ValidCoord of float | InvalidCoord    
-    type Coords =  ValidCoords of (float * float) | InvalidCoords
+    type Coords =  ValidCoords of (float * float) | InvalidCoords    
 
     let toListOfPositionParts (posistion:string) = 
         Regex
-            .Replace(posistion,"[^.\w]", " ")
+            .Replace(posistion,"[^.\w-]", " ")
             .Split([|' '|], StringSplitOptions.RemoveEmptyEntries) |> List.ofArray 
 
-    let toPosition pos = function
-    | ValidCoord degs::ValidCoord mins::ValidCoord secs::[ValidCoord sign] ->
-         pos {degrees=float degs;minutes=float mins; seconds=float secs; sign=sign}
+    let toPosition pos sign = function
+    | ValidCoord degs::ValidCoord mins::[ValidCoord secs]->
+         pos {degrees=int degs;minutes=int mins; seconds=float secs; sign=sign}
     | _ -> InvalidPosition
 
-    let parsePositionPart  posistionPart= 
-        match Double.TryParse (posistionPart) with
-        | true, res -> ValidCoord res
-        | false, _ -> InvalidCoord
+    let validateWholePositionPart min max parseFunc part = 
+        match parseFunc part with
+        | true, v when min <= v && v <= max -> Some v
+        | _ -> None
+    let (|ValidDegree|_|) max = validateWholePositionPart 0 max Int32.TryParse
+    let (|ValidMinutes|_|) part = validateWholePositionPart 0 59 Int32.TryParse part
+    let (|ValidSeconds|_|) part = validateWholePositionPart 0.0 59.99 Double.TryParse part
 
+    let parsePositionPart  = function             
+        | Degree (ValidDegree 180 degs, E) -> ValidCoord( float degs)
+        | Degree (ValidDegree 180 degs, W) -> ValidCoord( float degs)
+        | Degree (ValidDegree 90 degs, N) -> ValidCoord( float degs)
+        | Degree (ValidDegree 90 degs, S) -> ValidCoord( float degs)        
+        | Minute (ValidMinutes mins) -> ValidCoord(float mins)
+        | Second (ValidSeconds secs) -> ValidCoord secs
+        | _ -> InvalidCoord
+        
     let parsePositionParts = function
-    | degs::mins::secs::["N"] -> (List.map (parsePositionPart) [degs;mins;secs;"1.0"]) |> toPosition Lat
-    | degs::mins::secs::["S"] -> (List.map (parsePositionPart) [degs;mins;secs;"-1.0"]) |> toPosition Lat 
-    | degs::mins::secs::["E"] -> (List.map (parsePositionPart) [degs;mins;secs;"1.0"]) |> toPosition Lgt 
-    | degs::mins::secs::["W"] ->  (List.map (parsePositionPart) [degs;mins;secs;"-1.0"]) |> toPosition Lgt 
+    | degs::mins::secs::["N"] -> 
+        (List.map parsePositionPart [Degree (degs, N);Minute mins;Second secs]) |> toPosition Lat 1.0
+    | degs::mins::secs::["S"] -> 
+        List.map parsePositionPart [Degree (degs, S);Minute mins;Second secs] |> toPosition Lat (-1.0)
+    | degs::mins::secs::["E"] -> 
+        List.map parsePositionPart [Degree (degs, E);Minute mins;Second secs] |> toPosition Lgt 1.0
+    | degs::mins::secs::["W"] ->  
+        List.map parsePositionPart [Degree (degs, W);Minute mins;Second secs] |> toPosition Lgt (-1.0)
     | _ -> InvalidPosition
 
     let parsePosition = (toListOfPositionParts >> parsePositionParts) 
 
     let getTotalSeconds {degrees=deg; minutes=min; seconds=sec; sign=sign} = 
-        ((sec + 60.0 * min + 60.0 * 60.0 * deg) * sign)
+        ((sec + 60.0 * float min + 60.0 * 60.0 * float deg) * sign)
     let parseCoordinates (latStr, lgtStr) = 
         match parsePosition latStr, parsePosition lgtStr with
         | Lat lat, Lgt lgt -> ValidCoords (getTotalSeconds lat, getTotalSeconds lgt)
@@ -44,13 +62,16 @@ module E =
         
     let getPosition position = 
         let regex = @"(\s?(?:[0-9]*\.*[0-9]*[^\w]*){3} [E|W|S|N])*"
-        let m = Regex.Match (position, regex)
-        let captures = captureList m 1
+        let m = Regex.Match (position, regex) 
+        // let m = Regex.Match ( """14*-59'59.03" S 55*1'47" W""", regex)        
+        let captures = captureList m 1        
+        // List.map parsePosition captures
         match captures with
         | lat::[lgt] -> parseCoordinates (lat, lgt)
         | _ -> InvalidCoords
 
 module Client = 
-    match E.getPosition """14'27"35.03"' N 55'13"47"' E""" with
+    
+    match E.getPosition """14*-59'59.03" S 55*1'47" W""" with
     | E.InvalidCoords -> printfn "Invalid"
     | E.ValidCoords (lat, lgt) -> printfn "%A %A" lat lgt
