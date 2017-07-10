cac54019bb03518e5430f92703e88522c70ea948
Unknown <stefanpu13@gmail.com>
2017-07-10 18:52:01 +0300

    Add example of "mySeq" computation expression

--- 12 Computation expressions/12ComputationExpressions/12ComputationExpressions.fs
+++ 12 Computation expressions/12ComputationExpressions/12ComputationExpressions.fs
@@ -1,3 +1,21 @@
 module ComputationExpressions
 
 type mySeq<'a> = seq<'a>
+
+type MySeqClass() =
+    member bld.Yield a: mySeq<'a> = Seq.singleton a
+    member bld.For(sq:mySeq<'a>, f:'a -> mySeq<'b>):mySeq<'b> = 
+        Seq.collect f sq
+
+let mySeq = MySeqClass()
+
+let cartesian sqx sqy =  mySeq {
+    for x in sqx do
+        for y in sqy do
+            yield (x,y) 
+}
+
+let cartesian2 seqx seqy = 
+    Seq.collect (fun elx -> 
+        Seq.map (fun ely -> elx, ely) seqy
+    ) seqx
