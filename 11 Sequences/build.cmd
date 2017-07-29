43568e8bb1a572e91f7ef7af8e49c79c6aa3bea5
stefanpu <stefanpu13@gmail.com>
2017-07-29 22:58:46 +0300

    Auto stash before merge of "master" and "origin/master"

--- 11 Sequences/build.cmd
+++ 11 Sequences/build.cmd
@@ -5,7 +5,7 @@ cls
 if errorlevel 1 (
   exit /b %errorlevel%
 )
-REM Can use the following command instead "restore": .paket\paket.exe install --createnewbindingfiles
+REM Can use the following command instead "restore": .paket\paket.exe install --create-new-binding-files
 REM  flag "--createnewbindingfiles" is needed to redirect 3rd party deps that use older versions of FSharpCore
 .paket\paket.exe restore
 if errorlevel 1 (
